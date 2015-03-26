e5cdb9bad1c944e880e9c267965353f1995e9be1
Edward Wenger <ewenger@intven.com>
2015-03-26 16:05:41 -0700

    Missed file in previous commit

--- dtk/utils/analyzers/select.py
+++ dtk/utils/analyzers/select.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
+def example_selection(start_date='1/1/2000'):
     def f(timeseries):
         # N.B. defaults to once-per-week snapshots, not weekly (running) averages...
+        weekly=timeseries[::7]
+        dates=pd.date_range(start_date, periods=len(weekly),freq='W')
         return pd.Series(weekly,index=dates)
     return f
