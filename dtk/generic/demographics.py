1a51e3b93a8a5c4df8b30d538ee0a9c5ecff8fc1
Edward Wenger <wenger.edward@gmail.com>
2015-05-12 11:50:41 -0700

    Move malaria immune initialization
    - Move immune initialization functions from generic.demographics to
    malaria.immunity
    - Tweak demographics modification functions to be closer to new style
    (no compilation required, e.g.)

--- dtk/generic/demographics.py
+++ dtk/generic/demographics.py
@@ -1,50 +1,55 @@
 import os
 import json
-import subprocess
-import sys
-from dtk.tools.demographics.compiledemog import CompileDemographics
-from dtk.generic.geography import geographies
-
 
 params = {
-    "Demographics_Filename": "", 
+    "Demographics_Filename": "",
 
-    "Population_Scale_Type": "FIXED_SCALING", 
-    "Base_Population_Scale_Factor": 1, 
+    "Population_Scale_Type": "FIXED_SCALING",
+    "Base_Population_Scale_Factor": 1,
 
-    "Enable_Aging": 1, 
-    "Age_Initialization_Distribution_Type": "DISTRIBUTION_SIMPLE", 
+    "Enable_Aging": 1,
+    "Age_Initialization_Distribution_Type": "DISTRIBUTION_SIMPLE",
 
-    "Enable_Demographics_Birth": 1, 
-    "Enable_Demographics_Gender": 1, 
-    "Enable_Demographics_Initial": 1, 
-    "Enable_Demographics_Other": 1, 
-    "Enable_Demographics_Reporting": 1, 
+    "Enable_Demographics_Birth": 1,
+    "Enable_Demographics_Gender": 1,
+    "Enable_Demographics_Initial": 1,
+    "Enable_Demographics_Other": 1,
+    "Enable_Demographics_Reporting": 1,
 
-    "Enable_Immunity_Initialization_Distribution": 0, 
+    "Enable_Immunity_Initialization_Distribution": 0,
 
-    "Enable_Vital_Dynamics": 1, 
-    "Enable_Nondisease_Mortality": 1, 
-    "Enable_Birth": 1, 
-    "Birth_Rate_Dependence": "POPULATION_DEP_RATE", 
-    "x_Birth": 1, 
+    "Enable_Vital_Dynamics": 1,
+    "Enable_Nondisease_Mortality": 1,
+    "Enable_Birth": 1,
+    "Birth_Rate_Dependence": "POPULATION_DEP_RATE",
+    "x_Birth": 1,
 
-    "Enable_Disease_Mortality": 0, 
-    "Base_Mortality": 0, 
+    "Enable_Disease_Mortality": 0,
+    "Base_Mortality": 0,
     "Mortality_Time_Course": "DAILY_MORTALITY",
-    "x_Other_Mortality": 1, 
+    "x_Other_Mortality": 1,
 
-    "Individual_Sampling_Type": "TRACK_ALL", 
+    "Individual_Sampling_Type": "TRACK_ALL",
     "Base_Individual_Sample_Rate": 1,  ## all parameters below are unused without sampling
-    "Max_Node_Population_Samples": 40, 
-    "Sample_Rate_0_18mo": 1, 
-    "Sample_Rate_10_14": 1, 
-    "Sample_Rate_15_19": 1, 
-    "Sample_Rate_18mo_4yr": 1, 
-    "Sample_Rate_20_Plus": 1, 
-    "Sample_Rate_5_9": 1, 
+    "Max_Node_Population_Samples": 40,
+    "Sample_Rate_0_18mo": 1,
+    "Sample_Rate_10_14": 1,
+    "Sample_Rate_15_19": 1,
+    "Sample_Rate_18mo_4yr": 1,
+    "Sample_Rate_20_Plus": 1,
+    "Sample_Rate_5_9": 1,
     "Sample_Rate_Birth": 2
 }
+
+distribution_types = {
+    "CONSTANT_DISTRIBUTION" : 0,
+    "UNIFORM_DISTRIBUTION" : 1,
+    "GAUSSIAN_DISTRIBUTION" : 2,
+    "EXPONENTIAL_DISTRIBUTION" : 3,
+    "POISSON_DISTRIBUTION" : 4,
+    "LOG_NORMAL" : 5,
+    "BIMODAL_DISTRIBUTION" : 6
+    }
 
 def set_risk_mod(filename, distribution, par1, par2):
     set_demog_distributions(filename, [("Risk", distribution, par1, par2)])
@@ -52,55 +57,47 @@ def set_risk_mod(filename, distribution, par1, par2):
 def set_immune_mod(filename, distribution, par1, par2):
     set_demog_distributions(filename, [("Immunity", distribution, par1, par2)])
 
+def apply_to_defaults_or_nodes(demog,fn,*args):
+    # distributions may be in Defaults or in Nodes list
+    if "Defaults" in demog and "IndividualAttributes" in demog["Defaults"]:
+        fn(demog["Defaults"],*args)
+    else:
+        for node in demog["Nodes"]:
+            fn(node,*args)
+
 def set_demog_distributions(filename, distributions):
 
     with open( filename, "r" ) as demogjson_file:
         demog = json.loads( demogjson_file.read() )
 
-    distribution_types = { 
-        "CONSTANT_DISTRIBUTION" : 0,
-        "UNIFORM_DISTRIBUTION" : 1,
-        "GAUSSIAN_DISTRIBUTION" : 2,
-        "EXPONENTIAL_DISTRIBUTION" : 3,
-        "POISSON_DISTRIBUTION" : 4,
-        "LOG_NORMAL" : 5,
-        "BIMODAL_DISTRIBUTION" : 6
-        }
+    def set_attributes(d,dist_type,par1,par2):
+        d['IndividualAttributes'].update(
+            {dist_type + "DistributionFlag": distribution_types[distribution],
+             dist_type + "Distribution1": par1,
+             dist_type + "Distribution2": par2})
 
     for (dist_type, distribution, par1, par2) in distributions:
         if distribution not in distribution_types.keys():
             raise Exception("Don't recognize distribution type %s" % distribution)
 
-        # distributions may be in Nodes list or Defaults
-        if "Defaults" in demog and "IndividualAttributes" in demog["Defaults"]:
-            demog["Defaults"]["IndividualAttributes"][dist_type + "DistributionFlag"] = distribution_types[distribution]
-            demog["Defaults"]["IndividualAttributes"][dist_type + "Distribution1"] = par1
-            demog["Defaults"]["IndividualAttributes"][dist_type + "Distribution2"] = par2
-        else:
-            for node in demog["Nodes"]:
-                node["IndividualAttributes"][dist_type + "DistributionFlag"] = distribution_types[distribution]
-                node["IndividualAttributes"][dist_type + "Distribution1"] = par1
-                node["IndividualAttributes"][dist_type + "Distribution2"] = par2
+        apply_to_defaults_or_nodes(demog,set_attributes,dist_type,par1,par2)
 
-    # write
     with open( filename, "w" ) as output_file:
         output_file.write( json.dumps( demog, sort_keys=True, indent=4 ) )
 
-# Static demographics
-def set_static_demographics(input_path, config, recompile=False):
+def set_static_demographics(cb,use_existing=False):
 
-    if not recompile:
-        config["parameters"]["Birth_Rate_Dependence"] = "FIXED_BIRTH_RATE"
-        config["parameters"]["Demographics_Filename"] = config["parameters"]["Demographics_Filename"].replace("demographics","demographics.static",1)
-        return config
+    demog_filename=cb.get_param('Demographics_Filename')
+    static_demog_filename=demog_filename.replace("demographics","demographics.static",1)
+    cb.set_param("Demographics_Filename",static_demog_filename)
+    cb.set_param("Birth_Rate_Dependence","FIXED_BIRTH_RATE")
 
-    # get demographics file
-    demog_filename = config["parameters"]["Demographics_Filename"].replace("compiled.","",1)
-    with open( os.path.join(input_path, demog_filename), "r" ) as demogjson_file:
+    if use_existing:
+        return
+
+    with open( demog_filename, "r" ) as demogjson_file:
         demog = json.loads( demogjson_file.read() )
 
-    # set birth rate (by population) and death rate
-    config["parameters"]["Birth_Rate_Dependence"] = "FIXED_BIRTH_RATE"
     birthrate = 0.12329
     exponential_age_param = 0.000118
     population_removal_rate = 45
@@ -122,43 +119,32 @@ def set_static_demographics(input_path, config, recompile=False):
                     ]
                 }
 
-    # distributions may be in Nodes list or Defaults
-    if "Defaults" in demog and "IndividualAttributes" in demog["Defaults"]:
-        demog["Defaults"]["IndividualAttributes"]["MortalityDistribution"].update(mod_mortality)
-        demog["Defaults"]["IndividualAttributes"]["AgeDistribution1"] = exponential_age_param
-        demog["Defaults"]["NodeAttributes"]["BirthRate"] = birthrate
-    else:
-        for node in demog["Nodes"]:
-            node["IndividualAttributes"]["MortalityDistribution"].update(mod_mortality)
-            node["IndividualAttributes"]["AgeDistribution1"] = exponential_age_param
-            node["NodeAttributes"]["BirthRate"] = birthrate
+    def set_attributes(d):
+        d['IndividualAttributes'].update(
+            {"MortalityDistribution": mod_mortality,
+             "AgeDistributionFlag": distribution_types["EXPONENTIAL_DISTRIBUTION"]
+             "AgeDistribution1": exponential_age_param})
+        d['NodeAttributes'].update({"BirthRate": birthrate})
 
-    # write
-    output_file_path = os.path.join( input_path, demog_filename.replace(".json",".static.json",1))
+    apply_to_defaults_or_nodes(demog,set_attributes)
+
+    output_file_path = demog_filename.replace(".json",".static.json",1)
     with open( output_file_path, "w" ) as output_file:
         output_file.write( json.dumps( demog, sort_keys=True, indent=4 ) )
 
-    # compile
-    CompileDemographics(output_file_path, forceoverwrite=True)
-    config["parameters"]["Demographics_Filename"] = config["parameters"]["Demographics_Filename"].replace("demographics","demographics.static",1)
+def set_growing_demographics(cb,use_existing=False):
 
-    return config
+    demog_filename=cb.get_param('Demographics_Filename')
+    static_demog_filename=demog_filename.replace("demographics","demographics.pop_growth",1)
+    cb.set_param("Demographics_Filename",static_demog_filename)
+    cb.set_param("Birth_Rate_Dependence","POPULATION_DEP_RATE")
 
-# Realistic growing population demographics
-def set_realistic_demographics(input_path, config, recompile=False):
+    if use_existing:
+        return
 
-    if not recompile:
-        config["parameters"]["Birth_Rate_Dependence"] = "POPULATION_DEP_RATE"
-        config["parameters"]["Demographics_Filename"] = config["parameters"]["Demographics_Filename"].replace("demographics","demographics.pop_growth",1)
-        return config
-
-    # get demographics file
-    demog_filename = config["parameters"]["Demographics_Filename"].replace("compiled.","",1)
-    with open( os.path.join(input_path, demog_filename), "r" ) as demogjson_file:
+    with open( demog_filename, "r" ) as demogjson_file:
         demog = json.loads( demogjson_file.read() )
 
-    # set birth rate (by population) and death rate
-    config["parameters"]["Birth_Rate_Dependence"] = "POPULATION_DEP_RATE"
     birthrate = 0.0001
     mod_mortality = {
                     "NumDistributionAxes": 2,
@@ -178,51 +164,12 @@ def set_realistic_demographics(input_path, config, recompile=False):
                     ]
                 }
 
-    # distributions may be in Nodes list or Defaults
-    if "Defaults" in demog and "IndividualAttributes" in demog["Defaults"]:
-        demog["Defaults"]["IndividualAttributes"]["MortalityDistribution"].update(mod_mortality)
-        demog["Defaults"]["NodeAttributes"]["BirthRate"] = birthrate
-    else:
-        for node in demog["Nodes"]:
-            node["IndividualAttributes"]["MortalityDistribution"].update(mod_mortality)
-            node["NodeAttributes"]["BirthRate"] = birthrate
+    def set_attributes(d):
+        d['IndividualAttributes'].update({"MortalityDistribution": mod_mortality})
+        d['NodeAttributes'].update({"BirthRate": birthrate})
 
-    # write
-    output_file_path = os.path.join( input_path, demog_filename.replace(".json",".pop_growth.json",1))
+    apply_to_defaults_or_nodes(demog,set_attributes)
+
+    output_file_path = demog_filename.replace(".json",".static.json",1)
     with open( output_file_path, "w" ) as output_file:
         output_file.write( json.dumps( demog, sort_keys=True, indent=4 ) )
-
-    # compile
-    CompileDemographics(output_file_path, forceoverwrite=True)
-    config["parameters"]["Demographics_Filename"] = config["parameters"]["Demographics_Filename"].replace("demographics","demographics.pop_growth",1)
-
-    return config
-
-def add_immune_overlays(cb, tags):
-
-    demog_filename = cb.get_param("Demographics_Filename")
-
-    if len(demog_filename.split(';')) > 1:
-        raise Exception('add_immune_init function is expecting only a single demographics file.  Not a semi-colon-delimited list.')
-    split_demog = demog_filename.split('.')
-    prefix,suffix = split_demog[0],split_demog[1:]
-
-    demogfiles = [demog_filename]
-    for tag in tags:
-        if 'demographics' not in prefix:
-            raise Exception('add_immune_init function expecting a base demographics layer with demographics in the name.')
-        immune_init_file_name = prefix.replace("demographics","immune_init_" + tag, 1) + "."+suffix[0]
-        demogfiles.append(immune_init_file_name)
-
-    cb.update_params({ "Enable_Immunity_Initialization_Distribution":1,
-                       "Demographics_Filename": ';'.join(demogfiles) })
-    
-    
-    
-
-# Immune initialization based on habitat scaling
-def add_immune_init(cb, site, x_temp_habitats):
-    tags = []
-    for x_temp_habitat in x_temp_habitats:
-        tags.append( "x_" + str(x_temp_habitat) )
-    add_immune_overlays(cb, tags)
--- dtk/malaria/immunity.py
+++ dtk/malaria/immunity.py
@@ -1,20 +1,20 @@
 params = {
-    "Antibody_CSP_Decay_Days": 90, 
-    "Antibody_CSP_Killing_Inverse_Width": 1.5, 
-    "Antibody_CSP_Killing_Threshold": 20, 
+    "Antibody_CSP_Decay_Days": 90,
+    "Antibody_CSP_Killing_Inverse_Width": 1.5,
+    "Antibody_CSP_Killing_Threshold": 20,
 
     "Innate_Immune_Variation_Type": "NONE",
     "Pyrogenic_Threshold": 1.5e4,
-    "Fever_IRBC_Kill_Rate": 1.4, 
+    "Fever_IRBC_Kill_Rate": 1.4,
 
-    "Max_MSP1_Antibody_Growthrate": 0.045, 
-    "Antibody_Capacity_Growth_Rate": 0.09, 
+    "Max_MSP1_Antibody_Growthrate": 0.045,
+    "Antibody_Capacity_Growth_Rate": 0.09,
     "Nonspecific_Antibody_Growth_Rate_Factor": 0.5, # multiplied by major-epitope number to get rate
-    "Antibody_Stimulation_C50": 30, 
-    "Antibody_Memory_Level": 0.34, 
-    "Min_Adapted_Response": 0.05, 
+    "Antibody_Stimulation_C50": 30,
+    "Antibody_Memory_Level": 0.34,
+    "Min_Adapted_Response": 0.05,
 
-    "Cytokine_Gametocyte_Inactivation": 0.01667, 
+    "Cytokine_Gametocyte_Inactivation": 0.01667,
 
     "Maternal_Antibodies_Type": "SIMPLE_WANING",
     "Maternal_Antibody_Protection": 0.1327,
@@ -22,3 +22,30 @@ params = {
 
     "Erythropoiesis_Anemia_Effect": 3.5
     }
+
+def add_immune_overlays(cb, tags):
+
+    demogfiles = cb.get_param("Demographics_Filenames")
+
+    if len(demogfiles) > 1:
+        raise Exception('add_immune_init function is expecting only a single demographics file.')
+
+    demog_filename=demogfiles[0]
+    split_demog = demog_filename.split('.')
+    prefix,suffix = split_demog[0],split_demog[1:]
+
+    for tag in tags:
+        if 'demographics' not in prefix:
+            raise Exception('add_immune_init function expecting a base demographics layer with demographics in the name.')
+        immune_init_file_name = prefix.replace("demographics","immune_init_" + tag, 1) + "."+suffix[0]
+        demogfiles.append(immune_init_file_name)
+
+    cb.update_params({ "Enable_Immunity_Initialization_Distribution":1,
+                       "Demographics_Filenames": demogfiles })
+
+# Immune initialization based on habitat scaling
+def add_immune_init(cb, site, x_temp_habitats):
+    tags = []
+    for x_temp_habitat in x_temp_habitats:
+        tags.append( "x_" + str(x_temp_habitat) )
+    add_immune_overlays(cb, tags)
