4b6acb897d04097ba8fb0c37b381728953abb667
Benoit Raybaud <braybaud@intven.com>
2016-01-17 17:11:03 -0800

    Hotfix: Restore the Simulation_Duration in the site_clinical_incidence_cohort as the previous value was for quick local testing

--- dtk/tools/calibration/calibtool/study_sites/site_clinical_incidence_cohort.py
+++ dtk/tools/calibration/calibtool/study_sites/site_clinical_incidence_cohort.py
@@ -4,7 +4,7 @@ fine_age_bins = [ 0.08333, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                   11, 12, 13, 14, 15, 20, 25, 30, 40, 50, 60, 100 ]
 
 setup_functions = [ config_setup_fn(duration=365),
-                    summary_report_fn(age_bins=fine_age_bins, interval=360),
+                    summary_report_fn(age_bins=fine_age_bins, interval=21915),
                     lambda cb : cb.update_params({ "Geography": "Calibration",
                                                    "Demographics_Filenames": ["Calibration/birth_cohort_demographics.compiled.json"], 
                                                    'Base_Population_Scale_Factor' : 10,
