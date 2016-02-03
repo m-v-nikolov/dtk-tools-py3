52b14938fdd07f55dc0043a395c630b0053eb4a4
Benoit Raybaud <braybaud@intven.com>
2016-02-03 08:50:17 -0800

    Revert Dielmo test data. Update gitignore

--- dtk/tools/calibration/calib_example/study_sites/site_Dielmo.py
+++ dtk/tools/calibration/calib_example/study_sites/site_Dielmo.py
@@ -1,10 +1,11 @@
 import site_clinical_incidence_cohort
 
 setup_functions = site_clinical_incidence_cohort.get_setup_functions('Dielmo')
+
 reference_data = {        "annual_clinical_incidence_by_age" : {
             "n_obs" : [55, 60, 55, 50, 50, 38, 38, 38, 38, 38, 26, 26, 26, 26, 26, 110, 75, 75, 150, 70, 70, 90],
             "age_bins" : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 25, 30, 40, 50, 60, 100],
-            "Annual Clinical Incidence by Age Bin" : [1.2, 54, 6.1, 14.75, 13.1, 122.75, 12.7,11.9, 10.12, 0.8, 0.5, 0.25, 0.1, 10.2, 10.4, 10.3, 0.2, 0.2, 0.2, 0.15, 0.15, 0.15]
+            "Annual Clinical Incidence by Age Bin" : [1.2, 5, 6.1, 4.75, 3.1, 2.75, 2.7, 1.9, 0.12, 0.8, 0.5, 0.25, 0.1, 0.2, 0.4, 0.3, 0.2, 0.2, 0.2, 0.15, 0.15, 0.15]
         }
 }
 
