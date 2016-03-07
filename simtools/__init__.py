55d765ddb666ec575616552ab8d7ef6358538b98
Edward Wenger <wenger.edward@gmail.com>
2016-03-07 15:53:44 -0800

    Add COMPS_REST_HOST environment variable
    To interface nicely with latest pyCOMPS changes, allowing Tkinter
    pop-up of COMPS password prompt, pre-empting the Java version.

--- simtools/__init__.py
+++ simtools/__init__.py
@@ -1,4 +1,9 @@
+import os
+
+from SetupParser import SetupParser
+
 try:
+    os.environ['COMPS_REST_HOST'] = SetupParser().get('HPC', 'server_endpoint') + '/'
     from COMPSJavaInterop import COMPSJavaInterop
 except ImportError:
     print('Failed loading COMPSJavaInterop package; you will only be able to run local simulations.')
---
+++ test/test_import.py
@@ -0,0 +1,14 @@
+
+#### BAD ####
+
+import matplotlib.pyplot as plt
+import simtools
+
+# maybe interference with:
+# from Tk import *
+
+
+#### GOOD ####
+
+import simtools
+import matplotlib.pyplot as plt
