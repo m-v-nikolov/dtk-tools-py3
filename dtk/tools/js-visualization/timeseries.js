a8133041311d310d3d0b01bc2ba2e60a7aa6858a
m-v-nikolov <mnikolov@intven.com>
2016-04-05 16:00:54 -0700

    Timeseries known bug fixes - "special" timeseries scale dynamically within chart area - legend entries interaction on mouseover/click is fixed - svg clipping paths are fixed so that elements outside displayed time range are invisible

--- dtk/tools/js-visualization/timeseries.js
+++ dtk/tools/js-visualization/timeseries.js
@@ -11,7 +11,7 @@ function load_timeseries(ts_id, ts_type, colors, special)
 {
 	
 	var margin = {top: 20, right: 350, bottom: 100, left: 50},
-    margin2 = { top: 430, right: 10, bottom: 20, left: 40 },
+    margin2 = { top: 430, right: 150, bottom: 20, left: 40 },
     width = 1100 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
     height2 = 500 - margin2.top - margin2.bottom;
@@ -71,7 +71,7 @@ function load_timeseries(ts_id, ts_type, colors, special)
 	
 	//slider
 	  
-	var context = svg.append("g") // Brushing context box container
+	var context = svg.append("g") // brushing context box container
 	    .attr("transform", "translate(" + 0 + "," + 410 + ")")
 	    .attr("class", "context");
 	
@@ -97,6 +97,11 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		
 		  var categories = color.domain().map(function(name) { // Nest the data into an array of objects with new keys
 		
+			var isVisible = true;
+		      
+		    if(name !== "Special")
+		    	isVisible = false; 
+		      
 		    return {
 		      name: name, // "name": the tsv headers except date
 		      values: data.map(function(d) { // "values": which has an array of the times and values
@@ -105,17 +110,28 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		          ts_entry: +(d[name]),
 		          };
 		      }),
-		      visible: (name === "Special" ? true : false) 
+		      visible: isVisible 
 		    };
 		  });
 		
-		  xScale.domain(d3.extent(data, function(d) { return d.date; })); // extent = highest and lowest points, domain is data, range is bounding box
+		  xScale.domain(d3.extent(data, function(d) { return d.date; })); 
 		
 		  yScale.domain([0, d3.max(categories, function(c) { return d3.max(c.values, function(v) { return v.ts_entry; }); })]);
 		
-		  xScale2.domain(xScale.domain()); 
+		  xScale2.domain(xScale.domain());
+		  
+		  var special_ts;
+		  for(var i = 0; i < categories.length; i ++)
+			 if (categories[i].name == special) 
+			 {
+				 //alert("found special");
+				  special_ts = categories[i].values;
+				 //alert(special_ts.length);
+				  break;
+			 }
+			 
 		 
-		 //for slider part-----------------------------------------------------------------------------------
+		 //for slider part
 		
 		 var brush = d3.svg.brush()//for slider bar at the bottom
 		    .x(xScale2) 
@@ -133,7 +149,7 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		    .y1(0); // Top line of area, 0 (area chart not inverted)
 		
 		  //plot the rect as the bar at the bottom
-		  context.append("path") // Path is created using svg.area details
+		  context.append("path") 
 		    .attr("class", "area")
 		    .attr("d", contextArea(categories[0].values)) // pass first categories data .values to area path generator 
 		    .attr("fill", "#F1F1F2");
@@ -145,7 +161,7 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		    .selectAll("rect")
 		    .attr("height", height2) // Make brush rects same height 
 		      .attr("fill", "#E6E7E8");  
-		  //end slider part-----------------------------------------------------------------------------------
+		  //end slider part
 		
 		  // draw line graph
 		  svg.append("g")
@@ -165,55 +181,56 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		      .text(ts_type);
 		
 		  var category = svg.selectAll(".category")
-		      .data(categories) // Select nested data and append to new svg group elements
+		      .data(categories) // select nested data and append to new svg group elements
 		    .enter().append("g")
 		      .attr("class", "category");   
 		
 		  category.append("path")
 		      .attr("class", "line")
-		      .style("pointer-events", "none") // Stop line interferring with cursor
+		      .style("pointer-events", "none") // stop line interferring with cursor
 		      .attr("id", function(d) {
 		        return "line-" + d.name.replace(" ", "").replace("/", "");
 		      })
 		      .attr("d", function(d) { 
 		    	  
-		        	 if(d.name === special && d.visible)
-		        	 {
-		        		 svg.selectAll(".dot")
-		        		 .data(d.values.filter(function(d) {return !isNaN(d.ts_entry);}))
-		        		  .enter().append("circle")
-		        		    .attr("class", "dot")
-		        		    .attr("cx", line.x())
-		        		    .attr("cy", line.y())
-		        		    .attr("fill", color(d.name))
-		        		    .attr("stroke", "#000")
-		        		    .attr("stroke-width", "1.5px")
-		        		    .attr("r", 5);
-		        	 }
-		        	 else
-		        		 return d.visible ? line(d.values) : null; // If d.visible is true then draw line
+		    	  	 // draw dots instead of a line if the timeseries is "special" 
+		    	 if(d.name === special) 
+	        	 {
+	        		 if(d.visible)
+	        			 draw_special(svg, d.values, line, special);
+	        		 else
+	        			 remove_special(svg);
+	        	 }	
+		         else
+		         {
+		        	remove_special(svg);
+		        	draw_special(svg, special_ts, line, special); // redraw special in case yaxis is rescaled
+		        	return d.visible ? line(d.values) : null; // if d.visible is true then draw line
+		         }
 		      })
 		      .attr("clip-path", "url(#clip)")//use clip path to make irrelevant part invisible
 		      .style("stroke", function(d) { return color(d.name); });
 		
 		  // draw legend
-		  var legendSpace = 450 / categories.length; // 450/number of categories
+		  var legendSpace = 450 / categories.length; // expose legend size as parameter?
 		
-		  category.append("rect")
-		      .attr("width", 10)
-		      .attr("height", 10)                                    
-		      .attr("x", width + (margin.right/3) - 15) 
-		      .attr("y", function (d, i) { return (legendSpace)+i*(legendSpace) - 8; })  // spacing
+		  category.append("circle")
+		      //.attr("width", 20)
+		      //.attr("height", 20)
+		  	  .attr("r",20)
+		      .attr("cx", width + (margin.right/3) - 25) 
+		      .attr("cy", function (d, i) { return (legendSpace)+i*(legendSpace) - 4; })  // spacing
 		      .attr("fill",function(d) {
-		        return d.visible ? color(d.name) : "#F1F1F2"; // If array key "visible" = true then color rect, if not then make it grey 
+		        return d.visible ? color(d.name) : "#F1F1F2";  
 		      })
+		      //.attr("stroke-width", 20)
 		      .attr("class", "legend-box")
 		
-		      .on("click", function(d){ // On click make d.visible 
-		        d.visible = !d.visible; // If array key for this data selection is "visible" = true then make it false, if false then make it true
-		
-		        maxY = findMaxY(categories); // Find max Y timeseries value categories data with "visible"; true
-		        yScale.domain([0,maxY]); // Redefine yAxis domain based on highest y value of categories data with "visible"; true
+		      .on("click", function(d){ 
+		        d.visible = !d.visible;
+		        
+		        maxY = findMaxY(categories); // find max Y timeseries value categories data with "visible"; true
+		        yScale.domain([0,maxY]); // redefine yAxis domain based on highest y value of categories data with "visible"; true
 		        svg.select(".y.axis")
 		          .transition()
 		          .call(yAxis);   
@@ -221,22 +238,22 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		        category.select("path")
 		          .transition()
 		          .attr("d", function(d){
-		        	  
-		        	 if(d.name === special && d.visible)
-		        	 {
-		        		 svg.selectAll(".dot")
-		        		 .data(d.values.filter(function(d) {return !isNaN(d.ts_entry);}))
-		        		  .enter().append("circle")
-		        		    .attr("class", "dot")
-		        		    .attr("cx", line.x())
-		        		    .attr("cy", line.y())
-		        		    .attr("fill", color(d.name))
-		        		    .attr("stroke", "#000")
-		        		    .attr("stroke-width", "1.5px")
-		        		    .attr("r", 5);
-		        	 }
+		        	 // TODO: pull out the if statement block in a helper function
+		        	  // (the exact code is repeated above and below as well)
+		        	 if(d.name === special) 
+		        	 {		        		 
+		        		 if(d.visible)
+		        			 draw_special(svg, d.values, line, special);
+		        		 else
+		        			 remove_special(svg);
+		        	 }	
 		        	 else
-		        		 return d.visible ? line(d.values) : null; // If d.visible is true then draw line for this d selection
+		        	 {
+	                    remove_special(svg);
+		        	  	draw_special(svg, special_ts, line, special); // redraw special in case yaxis is rescaled; (this logic is not very good since redraw happens too frequently)
+		        	  	return d.visible ? line(d.values) : null; // If d.visible is true then draw line for this d selection
+		        	 }
+		        		 
 		          })
 		
 		        category.select("rect")
@@ -271,20 +288,19 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		      
 		  category.append("text")
 		      .attr("x", width + (margin.right/3)) 
-		      .attr("y", function (d, i) { return (legendSpace)+i*(legendSpace); })  // (return (11.25/2 =) 5.625) + i * (5.625) 
+		      .attr("y", function (d, i) { return (legendSpace)+i*(legendSpace); }) 
 		      .text(function(d) { return d.name; }); 
 		
-		  // Hover line 
 		  var hoverLineGroup = svg.append("g") 
 		            .attr("class", "hover-line");
 		
-		  var hoverLine = hoverLineGroup // Create line with basic attributes
+		  var hoverLine = hoverLineGroup 
 		        .append("line")
 		            .attr("id", "hover-line")
 		            .attr("x1", 10).attr("x2", 10) 
 		            .attr("y1", 0).attr("y2", height + 10)
-		            .style("pointer-events", "none") // Stop line interferring with cursor
-		            .style("opacity", 1e-6); // Set opacity to zero 
+		            .style("pointer-events", "none") // stop line interferring with cursor
+		            .style("opacity", 1e-6); 
 		
 		  var hoverDate = hoverLineGroup
 		        .append('text')
@@ -307,7 +323,8 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		        .attr("x", width + 20) // position tooltips  
 		        .attr("y", function (d, i) { return (legendSpace)+i*(legendSpace); }); // (return (11.25/2 =) 5.625) + i * (5.625) // position tooltips       
 		
-		  // Add mouseover events for hover line.
+		  // add mouseover events for hover line.
+		  /*
 		  d3.select("#mouse-tracker") // select chart plot background rect #mouse-tracker
 		  .on("mousemove", mousemove) // on mousemove activate mousemove function defined below
 		  .on("mouseout", function() {
@@ -317,12 +334,13 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		      d3.select("#hover-line")
 		          .style("opacity", 1e-6); // On mouse out making line invisible
 		  });
+		  */
 		
 		  function mousemove() { 
 			  
 		      var mouse_x = d3.mouse(this)[0]; // Finding mouse x position on rect
 		      var graph_x = xScale.invert(mouse_x); // get date
-		
+		      
 		      //var mouse_y = d3.mouse(this)[1]; // Finding mouse y position on rect
 		      //var graph_y = yScale.invert(mouse_y);
 		      //console.log(graph_x);
@@ -339,22 +357,17 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		      // Legend tooltips // http://www.d3noob.org/2014/07/my-favourite-tooltip-method-for-line.html
 		
 		      /* d3.mouse(this)[0] returns the x position on the screen of the mouse. 
-		      Next, takes the position on the screen and converts it into an equivalent date! */
+		      Next, take the position on the screen and convert it into an equivalent date */
 		      var x0 = xScale.invert(d3.mouse(this)[0]), 
 		      
-		      i = bisectDate(data, x0, 1), // use our bisectDate function that we declared earlier to find the index of our data array that is close to the mouse cursor
-		      /*It takes our data array and the date corresponding to the position of or mouse cursor and returns the index number of the data array which has a date that is higher than the cursor position.*/
+		      i = bisectDate(data, x0, 1), // use bisectDate function declared earlier to find the index of the data array that is close to the mouse cursor
+		      /*It takes the data array and the date corresponding to the position of mouse cursor and returns the index number of the data array which has a date that is higher than the cursor position.*/
 		      d0 = data[i - 1],
 		      d1 = data[i],
 		      /*d0 is the combination of time and value that is in the data array at the index to the left of the cursor and d1
 		       *  is the combination of date and close that is in the data array at the index to the right of the cursor. 
 		       *  In other words we now have two variables that know the value and date above and below the date that corresponds to the position of the cursor.*/
 		      d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-		      /*The final line in this segment declares a new array d that is represents the date and close combination that is closest to the cursor. 
-		       * If the distance between the mouse cursor and the date and close combination on the left is greater than the distance between the mouse
-		       *  cursor and the date and close combination on the right then d is an array of the date and close on the right of the cursor (d1). 
-		       *  Otherwise d is an array of the date and close on the left of the cursor (d0).
-		       *  */
 		
 		      //d is now the data row for the date closest to the mouse position
 		
@@ -369,37 +382,76 @@ function load_timeseries(ts_id, ts_type, colors, special)
 		  //for brusher of the slider bar at the bottom
 		  function brushed() {
 		
-		    xScale.domain(brush.empty() ? xScale2.domain() : brush.extent()); // If brush is empty then reset the Xscale domain to default, if not then make it the brush extent 
+		    xScale.domain(brush.empty() ? xScale2.domain() : brush.extent()); // if brush is empty then reset the Xscale domain to default, if not then make it the brush extent 
 		
 		    svg.select(".x.axis") // replot xAxis with transition when brush used
 		          .transition()
 		          .call(xAxis);
 		
-		    maxY = findMaxY(categories); // Find max Y value categories data with "visible"; true
-		    yScale.domain([0,maxY]); // Redefine yAxis domain based on highest y value of categories data with "visible"; true
-		    
-		    svg.select(".y.axis") // Redraw yAxis
+		    maxY = findMaxY(categories); // find max Y value categories data with "visible"; true
+		    yScale.domain([0,maxY]); // redefine yAxis domain based on highest y value of categories data with "visible"; true
+		    //alert(maxY);
+		    svg.select(".y.axis") // redraw yAxis
 		      .transition()
 		      .call(yAxis);   
 		
-		    category.select("path") // Redraw lines based on brush xAxis scale and domain
+		    category.select("path") // redraw lines based on brush xAxis scale and domain
 		      .transition()
 		      .attr("d", function(d){
-		          return d.visible ? line(d.values) : null; // If d.visible is true then draw line for this d selection
+		    	  if(d.name === special) 
+		          {
+		        		 if(d.visible)
+		        			 draw_special(svg, d.values, line, special);	 
+		        		 else
+		        			 remove_special(svg);
+		          }	
+		          else
+		          {
+	        	  	remove_special(svg);
+	        	  	draw_special(svg, special_ts, line, special); // redraw special in case yaxis is rescaled
+	        	  	return d.visible ? line(d.values) : null; // if d.visible is true then draw line for this d selection
+		          }
+		        		
 		      });
 		    
 		  };      
 	
 	}); // end load tsv block
 	  
-	  function findMaxY(data){  // Define function "findMaxY"
+	  function findMaxY(data){  
 	    var maxYValues = data.map(function(d) { 
 	      if (d.visible){
-	        return d3.max(d.values, function(value) { // Return max value
+	        return d3.max(d.values, function(value) { // return max-y value
 	          return value.ts_entry; })
 	      }
 	    });
 	    return d3.max(maxYValues);
+	  }
+	  
+	  
+	  function draw_special(svg, timeseries, line, special)
+	  {
+		  
+		  //alert("drawing special")
+		  //alert(timeseries.length)
+		  svg.selectAll(".dot")
+ 		 .data(timeseries.filter(function(d) { return !isNaN(d.ts_entry); }))
+ 		  .enter().append("circle")
+ 		    .attr("class", "dot")
+ 		    .attr("cx", line.x())
+ 		    .attr("cy", line.y())
+ 		    .attr("fill", color(special))
+ 		    .attr("stroke", "#000")
+ 		    .attr("stroke-width", "1.5px")
+ 		    .attr("r", 5)
+ 		    .attr("clip-path", "url(#clip)")//use clip path to make irrelevant part invisible;
+	  }
+	  
+	  function remove_special(svg)
+	  {
+		  //alert("removing special")
+		  svg.selectAll(".dot").remove();
+ 		 
 	  }
 	
 }
