e69d9ae40055cc11c1b18cf9a327305c2310fc4a
m-v-nikolov <mnikolov@intven.com>
2016-04-12 23:24:34 -0700

    - Wired timeseries widget via comms layer

--- dtk/tools/js-visualization/dashboard.js
+++ dtk/tools/js-visualization/dashboard.js
@@ -83,6 +83,7 @@ function load_node_charts(params)
 	{
 		node_label = params["NodeLabel"];
 		heatmap(node_label);
+		timeseries(node_label);
 	}
 }
 
@@ -104,6 +105,19 @@ function heatmap(node_label)
 					'zi', // what attribute of the heatmap json objects corresponds to colors (i.e. z axis values)
 					'.resourcecontainer.charts'
 	);
+}
+
+
+function timeseries(node_label)
+{
+	load_timeseries(
+			node_label, 
+			'prevalence', 
+			['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928'],
+			'observed',
+			".resourcecontainer.timeseries"
+		   );		
+
 }
 
 function load_maps(map_data_file)
