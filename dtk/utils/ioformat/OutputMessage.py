2ae28a22c3b635a32d1e3d515d7e01d6bb732407
Benoit Raybaud <braybaud@intven.com>
2016-05-27 11:20:45 -0700

    Add deprecation for Demographic_Filename according to #136

--- dtk/utils/ioformat/OutputMessage.py
+++ dtk/utils/ioformat/OutputMessage.py
@@ -1,37 +1,44 @@
-import sys 
+import sys
 
-class OutputMessage():
-    
-    def __init__(self, msg, style = 'regular'):
+
+class OutputMessage:
+    deprecation_list = list()
+
+    def __init__(self, msg, style='regular'):
 
         self.style = style
-        
+
         if self.style == 'bold':
             self.bold(msg)
-            
-        if self.style == 'regular':
+
+        elif self.style == 'regular':
             self.regular(msg)
-            
-        if self.style == 'flushed':
+
+        elif self.style == 'flushed':
             self.flushed(msg)
-        
-    
+
+        elif self.style == 'deprecate':
+            # Avoid warning the user multiple times
+            if msg not in OutputMessage.deprecation_list:
+                OutputMessage.deprecation_list.append(msg)
+                self.deprecate(msg)
+
     def bold(self, msg):
-        
         print
         print "=================================================="
         print msg.upper()
         print "=================================================="
         print
-        
-        
-        
+
+    def deprecate(self, msg):
+        print "/!\\ DEPRECATION WARNING /!\\"
+        print msg
+
     def regular(self, msg):
-        print 
+        print
         print msg
         print
-        
-    
+
     def flushed(self, msg):
         sys.stdout.write('\r')
         sys.stdout.write(msg)
