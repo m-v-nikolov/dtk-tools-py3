918368faac2f04ec4a86d51a66ee8eaef3bc1095
Svetlana Titova <stitova@intven.com>
2016-05-31 15:00:10 -0700

    Updating utils.py to write data to simtools/simtools.cfg

--- examples/notebooks/utils.py
+++ examples/notebooks/utils.py
@@ -11,7 +11,8 @@ from dtk.utils.core.DTKSetupParser import DTKSetupParser
 from simtools.ExperimentManager import ExperimentManagerFactory
 
 def write_dtk_config(max_sims, sim_root, input_root, bin_path, exe_path):
-    conf_path = os.path.join(site.getsitepackages()[1], 'dtk', 'dtk_setup.cfg')
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+    conf_path = os.path.join(currentDir, '..', '..', 'simtools', 'simtools.cfg')
     print conf_path
     config = ConfigParser.RawConfigParser()
     config.read(conf_path)
@@ -29,7 +30,7 @@ def write_dtk_config(max_sims, sim_root, input_root, bin_path, exe_path):
     if not os.path.exists(sim_root):
         os.mkdir(sim_root)
 
-    print "The dtk_config.cfg file has been successfully updated!"
+    print "The simtools.cfg file has been successfully updated!"
 
 
 def test_if_dtk_present():
