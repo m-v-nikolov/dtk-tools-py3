f8bf49b208b0433ec758d51a1a3f487a9e351fe0
Benoit Raybaud <braybaud@intven.com>
2016-09-02 11:50:17 -0700

    Update example_sweep.py

--- examples/example_sweep.py
+++ examples/example_sweep.py
@@ -3,21 +3,20 @@
 
 from dtk.utils.core.DTKConfigBuilder import DTKConfigBuilder
 from dtk.utils.builders.sweep import GenericSweepBuilder
-from dtk.vector.study_sites import configure_site
 from simtools.SetupParser import SetupParser
 
 # For example only -- Force the selected block to be EXAMPLE
-SetupParser("HPC")
+SetupParser("EXAMPLE")
 
-exp_name  = 'SweepForPerformanceTesting'
-builder = GenericSweepBuilder.from_dict({'Run_Number': range(100),
-                                         'Simulation_Duration':[365,3650,36500]})
+exp_name  = 'ExampleSweep'
+builder = GenericSweepBuilder.from_dict({'Run_Number': range(3),
+                                         '_site_': ['Namawala', 'Matsari']})
 
 cb = DTKConfigBuilder.from_defaults('MALARIA_SIM',
                                     Num_Cores=1,
                                     Base_Population_Scale_Factor=0.1,
-                                    x_Temporary_Larval_Habitat=0.05)
-configure_site(cb,'Namawala')
+                                    x_Temporary_Larval_Habitat=0.05,
+                                    Simulation_Duration=365*20)
 
 run_sim_args =  {'config_builder': cb,
                  'exp_name': exp_name,
@@ -29,5 +28,5 @@ if __name__ == "__main__":
     from simtools.SetupParser import SetupParser
     from simtools.ExperimentManager.ExperimentManagerFactory import ExperimentManagerFactory
 
-    sm = ExperimentManagerFactory.from_model(SetupParser().get('exe_path'), 'HPC')
+    sm = ExperimentManagerFactory.from_model(SetupParser().get('exe_path'), 'LOCAL')
     sm.run_simulations(**run_sim_args)
