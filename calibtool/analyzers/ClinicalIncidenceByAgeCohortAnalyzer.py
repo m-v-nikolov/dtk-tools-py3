9c57e7451063d5ad3da70015bb9f6ecb8e841e28
edwenger <wenger.edward@gmail.com>
2016-11-30 15:09:07 -0800

    Whitespace and renaming of test script.

--- calibtool/analyzers/ClinicalIncidenceByAgeCohortAnalyzer.py
+++ calibtool/analyzers/ClinicalIncidenceByAgeCohortAnalyzer.py
@@ -1,6 +1,3 @@
-
-# TODO: Generalize this style of CalibAnalyzer as much as possible
-#       to minimize repeated code in e.g. PrevalenceByAgeAnalyzer
 import logging
 
 import pandas as pd
@@ -10,6 +7,7 @@ from calibtool.analyzers.Helpers import accumulate_agebins_cohort
 from dtk.utils.analyzers.BaseAnalyzer import BaseAnalyzer
 
 logger = logging.getLogger(__name__)
+
 
 class ClinicalIncidenceByAgeCohortAnalyzer(BaseAnalyzer):
 
@@ -27,29 +25,29 @@ class ClinicalIncidenceByAgeCohortAnalyzer(BaseAnalyzer):
         self.site = None
 
     def set_site(self, site):
-        '''
+        """
         Get the reference data that this analyzer needs from the specified site.
-        '''
+        """
 
         self.site = site
         self.reference = self.site.reference_data['annual_clinical_incidence_by_age']
 
     def filter(self, sim_metadata):
-        '''
+        """
         This analyzer only needs to analyze simulations for the site it is linked to.
         N.B. another instance of the same analyzer may exist with a different site
              and correspondingly different reference data.
-        '''
+        """
         return sim_metadata.get('__site__', False) == self.site.name
 
     def apply(self, parser):
-        '''
+        """
         Extract data from output data and accumulate in same bins as reference.
 
         TODO: if we want to plot with the original analyzer age bins,
               we will also need to emit a separate table of parsed data:
               data['Annual Clinical Incidence by Age Bin'], data['Age Bins']
-        '''
+        """
 
         data = parser.raw_data[self.filenames[0]]
         ref_age_bins = self.reference['age_bins']
@@ -70,9 +68,9 @@ class ClinicalIncidenceByAgeCohortAnalyzer(BaseAnalyzer):
         return channel_data
 
     def combine(self, parsers):
-        '''
+        """
         Combine the simulation data into a single table for all analyzed simulations.
-        '''
+        """
 
         selected = [p.selected_data[id(self)] for p in parsers.values() if id(self) in p.selected_data]
         combined = pd.concat(selected, axis=1,
@@ -83,10 +81,10 @@ class ClinicalIncidenceByAgeCohortAnalyzer(BaseAnalyzer):
         logger.debug(self.data)
 
     def compare(self, sample):
-        '''
+        """
         Assess the result per sample, in this case the likelihood
         comparison between simulation and reference data.
-        '''
+        """
 
         sample['n_obs'] = self.reference['n_obs']
         sample['rates'] = self.reference['Annual Clinical Incidence by Age Bin']
@@ -101,17 +99,17 @@ class ClinicalIncidenceByAgeCohortAnalyzer(BaseAnalyzer):
             sample['Clinical Incidents'].tolist())
 
     def finalize(self):
-        '''
+        """
         Calculate the output result for each sample.
-        '''
+        """
         self.result = self.data.groupby(level='sample').apply(self.compare)
         logger.debug(self.result)
 
     def cache(self):
-        '''
+        """
         Return a cache of the minimal data required for plotting sample comparisons
         to reference comparisons.
-        '''
+        """
 
         cache = self.data.copy()
         cache = cache[cache['Person Years'] > 0]
@@ -124,5 +122,5 @@ class ClinicalIncidenceByAgeCohortAnalyzer(BaseAnalyzer):
         return {'sims': sample_dicts, 'reference': self.reference, 'axis_names': [self.x, self.y]}
 
     def uid(self):
-        ''' A unique identifier of site-name and analyzer-name. '''
+        """ A unique identifier of site-name and analyzer-name. """
         return '_'.join([self.site.name, self.name])
--- calibtool/analyzers/Helpers.py
+++ calibtool/analyzers/Helpers.py
@@ -3,11 +3,13 @@ import numpy as np
 import itertools
 import calendar
 
-def json_to_pandas(simdata,bins,channel):
-    '''
+
+def json_to_pandas(simdata, bins, channel):
+    """
     A function to convert simulation data from a json file to
     a Pandas dataframe
-    '''
+    """
+
     bin_tuples = list(itertools.product(*bins.values()))
     multi_index = pd.MultiIndex.from_tuples(bin_tuples, names=bins.keys())
 
@@ -17,10 +19,11 @@ def json_to_pandas(simdata,bins,channel):
 
 
 def ref_json_to_pandas(refdata, required_reference_types, bins, channel):
-    '''
+    """
     A function to convert reference data from the site_*.py
     to a Pandas dataframe
-    '''
+    """
+
     if 'by_age_and_season' in required_reference_types:
         Para = [[] for i in range(2)]
         for season in refdata['Seasons']:
@@ -35,10 +38,12 @@ def ref_json_to_pandas(refdata, required_reference_types, bins, channel):
 
     return ref_channel_series
 
-def reorder_sim_data(channel_series,ref_channel_series,months,channel,population):
-    '''
+
+def reorder_sim_data(channel_series, ref_channel_series, months, channel, population):
+    """
     A function to reorder sim data to match reference data
-    '''
+    """
+
     # Data frames
     df_pop = population.reset_index()
     df_sim = channel_series.reset_index()
@@ -58,8 +63,8 @@ def reorder_sim_data(channel_series,ref_channel_series,months,channel,population
     season_month = dict(zip(months,seasons))
 
     # Reorder simulation data to match reference data
-    df_sim = df_sim[df_sim.Time.isin(seasonIndex)]      # Only retain months corresponding to seasons in ref data
-    pop = pd.merge(df_sim, df_pop, left_on=['Time', 'Age Bins'], right_on=['Time', 'Age Bins']) # Find population by age bin and pull out relevant rows
+    df_sim = df_sim[df_sim.Time.isin(seasonIndex)]  # Only retain months corresponding to seasons in ref data
+    pop = pd.merge(df_sim, df_pop, left_on=['Time', 'Age Bins'], right_on=['Time', 'Age Bins'])  # Find population by age bin and pull out relevant rows
     df_sim['PfPR by Parasitemia and Age Bin'] = pop['PfPR by Parasitemia and Age Bin'].multiply(pop['Population by Age Bin']).values
     df_sim['Age Bins'] = pd.cut((df_sim['Time'] + 1) / 12, age_bins)  # Reorder Age Bins
     df_sim['Time'] = [season_month[calendar.month_name[i]] for i in (df_sim['Time']%12+1)]  # Label Season
@@ -70,45 +75,42 @@ def reorder_sim_data(channel_series,ref_channel_series,months,channel,population
     return df_sim
 
 
-
-
-
-#       this is copied wholesale from dtk.calibration.analyzers.analyze_clinical_incidence_by_age_cohort
-def accumulate_agebins_cohort(simdata, average_pop, sim_agebins, raw_agebins) :
-    '''
+def accumulate_agebins_cohort(simdata, average_pop, sim_agebins, raw_agebins):
+    """
     A function to sum over each year's values in a summary report,
     combining incidence rate and average population
     to give total counts and population in the reference age binning.
-    '''
+    """
 
+    glommed_data = [0] * len(raw_agebins)
+    simageindex = [-1] * len(sim_agebins)
+    yearageindex = [-1] * len(simdata)
+    num_in_bin = [0] * len(raw_agebins)
 
-    glommed_data = [0]*len(raw_agebins)
-    simageindex = [-1]*len(sim_agebins)
-    yearageindex = [-1]*len(simdata)
-    num_in_bin = [0]*len(raw_agebins)
-
-    for i in range(len(simageindex)) :
-        for j, age in enumerate(raw_agebins) :
-            if sim_agebins[i] > age :
+    for i in range(len(simageindex)):
+        for j, age in enumerate(raw_agebins):
+            if sim_agebins[i] > age:
                 simageindex[i] = j
                 break
-    for i in range(len(yearageindex)) :
-        for j, age in enumerate(raw_agebins) :
-            if i < age :
+
+    for i in range(len(yearageindex)):
+        for j, age in enumerate(raw_agebins):
+            if i < age:
                 yearageindex[i] = j
                 break
 
-    for i in range(len(yearageindex)) :
-        if yearageindex[i] < 0 :
+    for i in range(len(yearageindex)):
+        if yearageindex[i] < 0:
             continue
-        for j in range(len(simageindex)) :
-            if simageindex[j] < 0 :
+        for j in range(len(simageindex)):
+            if simageindex[j] < 0:
                 continue
-            glommed_data[simageindex[j]] += simdata[i][j]*average_pop[i][j]
+            glommed_data[simageindex[j]] += simdata[i][j] * average_pop[i][j]
             num_in_bin[simageindex[j]] += average_pop[i][j]
 
     return num_in_bin, glommed_data
-#
+
+
 # def accumulate_ageseasonbins_cohort(simdata, average_pop, raw_agebins, seasons) :
 #     '''
 #     A function to sum over each year's values in a summary report,
@@ -144,30 +146,34 @@ def accumulate_agebins_cohort(simdata, average_pop, sim_agebins, raw_agebins) :
 #                 num_in_bin[i,simageindex[i,j]] += average_pop[seasonindex[i,j]]
 #
 #     return num_in_bin, glommed_data
-#
-def get_spatial_report_data_at_date(sp_data, date) :
 
-    return pd.DataFrame( { 'node' : sp_data['nodeids'],
-                           'data' : sp_data['data'][date] } )
 
-def get_risk_by_distance(df_sim, distances, ddf) :
+def get_spatial_report_data_at_date(sp_data, date):
+
+    return pd.DataFrame({'node': sp_data['nodeids'],
+                         'data': sp_data['data'][date]})
+
+
+def get_risk_by_distance(df_sim, distances, ddf):
 
     nodelist = df_sim['node'].values.tolist()
     rel_risk = []
-    for k, n_dist in enumerate(distances) :
+
+    for k, n_dist in enumerate(distances):
         pos_w_pos = 0.
         tot_w_pos = 0.
-        for hh_num in df_sim.index :
-            if df_sim.ix[hh_num, 'pos'] < 1 :
+
+        for hh_num in df_sim.index:
+            if df_sim.ix[hh_num, 'pos'] < 1:
                 continue
 
-            if n_dist == 0 and df_sim.ix[hh_num, 'pop'] > 1 :
+            if n_dist == 0 and df_sim.ix[hh_num, 'pop'] > 1:
                 hh_pos = df_sim.ix[hh_num, 'pos']
                 hh_tot = df_sim.ix[hh_num, 'pop']
                 num_pos = (hh_pos - 1)*hh_pos
                 num_ppl = (hh_tot - 1)*hh_pos
 
-            else :
+            else:
                 # node IDs of nodes within distance
                 neighbors = ddf[(ddf['node1'] == nodelist[hh_num]) & (ddf['node2'] != nodelist[hh_num]) &
                                 (ddf['dist'] <= n_dist) & (ddf['dist'] > distances[k-1])]['node2'].values
@@ -178,8 +184,10 @@ def get_risk_by_distance(df_sim, distances, ddf) :
 
             pos_w_pos += num_pos
             tot_w_pos += num_ppl
-        if tot_w_pos > 0 :
+
+        if tot_w_pos > 0:
             rel_risk.append(pos_w_pos/tot_w_pos)
-        else :
+        else:
             rel_risk.append(0)
+
     return rel_risk
