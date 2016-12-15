332ea0df8f4c89b3fbd84984a02adbb1f5dd86cd
edwenger <wenger.edward@gmail.com>
2016-12-15 12:21:41 -0800

    Tidy up BasePlotter interfaces and derived classes. Give CalibManager reference to plotters in CalibManager.__init__ Fix incorrect treatment of lower-case raw_input in create_calibration Use DataFrames more deeply into SiteDataPlotter logic

--- calibtool/plotters/BasePlotter.py
+++ calibtool/plotters/BasePlotter.py
@@ -1,19 +1,53 @@
+import os
+import logging
 from abc import ABCMeta, abstractmethod
-from calibtool.utils import ResumePoint
+
+logger = logging.getLogger(__name__)
 
 
 class BasePlotter:
     __metaclass__ = ABCMeta
 
-    def __init__(self, combine_sites=True, prior_fn={}):
+    def __init__(self, combine_sites=True):
         self.combine_sites = combine_sites
-        self.prior_fn = prior_fn
-        self.all_results = None
-        self.directory = None
-        self.param_names = None
+
+        self.manager = None
         self.site_analyzer_names = None
 
+    def set_manager(self, manager):
+        self.manager = manager
+        self.site_analyzer_names = self.manager.site_analyzer_names()
+        return self
+
+    def get_iteration_directory(self):
+        return self.manager.iteration_directory()
+
+    def get_plot_directory(self):
+        return os.path.join(self.manager.name, '_plots')
+
+    @property
+    def all_results(self):
+        return self.manager.all_results
+
     @abstractmethod
-    def visualize(self, calib_manager):
+    def visualize(self):
         pass
 
+    @abstractmethod
+    def cleanup(self):
+        pass
+
+    @staticmethod
+    def combine_by_site(site_name, analyzer_names, results):
+        """
+        Sum the result values over analyzers performed on the specified site and add SITE_total to results
+        :param site_name: The name of the CalibSite
+        :param analyzer_names: A list of CalibAnalyzer names corresponding to the site
+        :param results: A pandas.DataFrame of results into which to add a combined site-analyzer-result column
+        :return: None
+        """
+        site_analyzers = [site_name + '_' + a for a in analyzer_names]
+        logger.debug('site_analyzers: %s', site_analyzers)
+        site_total = site_name + '_total'
+        results[site_total] = results[site_analyzers].sum(axis=1)
+        logger.debug('results[%s]=%s', site_total, results[site_total])
--- calibtool/plotters/SiteDataPlotter.py
+++ calibtool/plotters/SiteDataPlotter.py
@@ -3,11 +3,9 @@ import os
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import pandas as pd
 import seaborn as sns
 
 from calibtool.plotters.BasePlotter import BasePlotter
-from calibtool.visualize import combine_by_site
 
 sns.set_style('white')
 
@@ -15,68 +13,64 @@ logger = logging.getLogger(__name__)
 
 
 class SiteDataPlotter(BasePlotter):
-    def __init__(self, combine_sites=True, prior_fn={}):
-        super(SiteDataPlotter, self).__init__(combine_sites, prior_fn)
+    def __init__(self, combine_sites=True):
+        super(SiteDataPlotter, self).__init__(combine_sites)
 
-    def visualize(self, calib_manager):
-        self.all_results = calib_manager.all_results.reset_index()
-        logger.debug(self.all_results)
+    @property
+    def num_to_plot(self):
+        return self.manager.num_to_plot
 
-        # TODO: rethink when these are being set, e.g. why not pass calib_manager to __init__?
-        self.num_to_plot = calib_manager.num_to_plot
-        self.site_analyzer_names = calib_manager.site_analyzer_names()
-        self.state_for_iteration = calib_manager.state_for_iteration
-        self.plots_directory = os.path.join(calib_manager.name, '_plots')
+    @property
+    def directory(self):
+        return self.get_plot_directory()
 
-        # TODO: this is so we can call Analyzer.plot_sim and Analyzer.plot_ref
-        # TODO: if these are classmethods, we only need the name, not the instances
-        self.analyzers = {}
-        for site in calib_manager.sites:
+    def get_site_analyzer(self, site_name, analyzer_name):
+        for site in self.manager.sites:
+            if site_name != site.name:
+                continue
             for analyzer in site.analyzers:
-                self.analyzers[site.name+"_"+analyzer.name] = analyzer
+                if analyzer_name == analyzer.name:
+                    return analyzer
+        raise Exception('Unable to find analyzer=%s for site=%s' % (analyzer_name, site_name))
 
+    def get_analyzer_data(self, iteration, site_name, analyzer_name):
+        site_analyzer = '%s_%s' % (site_name, analyzer_name)
+        return self.manager.state_for_iteration(iteration).analyzers[site_analyzer]
+
+    def visualize(self):
         if self.combine_sites:
-            for site, analyzers in self.site_analyzer_names.items():
-                self.plot_analyzers(site, analyzers, self.all_results)
+            for site_name, analyzer_names in self.site_analyzer_names.items():
+                sorted_results = self.all_results.sort_values(by='total', ascending=False).reset_index()
+                self.plot_analyzers(site_name, analyzer_names, sorted_results)
         else:
-            for site, analyzers in self.site_analyzer_names.items():
-                combine_by_site(site, analyzers, self.all_results)
-                sorted_results = self.all_results.sort_values(by='%s_total' % site, ascending=False)
-                self.plot_analyzers(site, analyzers, sorted_results)
+            for site_name, analyzer_names in self.site_analyzer_names.items():
+                self.combine_by_site(site_name, analyzer_names, self.all_results)
+                sorted_results = self.all_results.sort_values(by='%s_total' % site_name, ascending=False).reset_index()
+                self.plot_analyzers(site_name, analyzer_names, sorted_results)
 
-    def get_iteration_samples(self, results, subset=False):
-        subset = results.iloc[:self.num_to_plot] if subset else results
-        samples = {k: {'sample': v['sample'].tolist(),
-                       'rank': v.index.tolist(),
-                       'result': v['total'].tolist()}
-                   for k, v in subset.groupby('iteration')}
-        logger.debug(samples)
-        return samples
-
-    def plot_analyzers(self, site, analyzers, samples):
+    def plot_analyzers(self, site_name, analyzer_names, samples):
         cmin, cmax = samples['total'].describe()[['min', 'max']].tolist()
         cmin = cmin if cmin < cmax else cmax - 1  # avoid divide by zero in color range
 
-        best_samples = self.get_iteration_samples(samples, subset=True)
-        all_samples = self.get_iteration_samples(samples)
-        for analyzer in analyzers:
-            site_analyzer = '%s_%s' % (site, analyzer)
+        for analyzer_name in analyzer_names:
+            site_analyzer = '%s_%s' % (site_name, analyzer_name)
             try:
-                os.makedirs(os.path.join(self.plots_directory, site_analyzer))
+                os.makedirs(os.path.join(self.directory, site_analyzer))
             except:
                 pass
-            self.plot_best(site_analyzer, best_samples)
-            self.plot_all(site_analyzer, all_samples, clim=(cmin, cmax))
 
-    def plot_best(self, site_analyzer, iter_samples):
+            self.plot_best(site_name, analyzer_name, samples.iloc[:self.num_to_plot])
+            self.plot_all(site_name, analyzer_name, samples, clim=(cmin, cmax))
 
-        analyzer = self.analyzers[site_analyzer]
+    def plot_best(self, site_name, analyzer_name, samples):
 
-        for iteration, samples in iter_samples.items():
-            analyzer_data = self.state_for_iteration(iteration).analyzers[site_analyzer]
+        analyzer = self.get_site_analyzer(site_name, analyzer_name)
 
-            for sample, rank in zip(samples['sample'], samples['rank']):
-                fname = os.path.join(self.plots_directory, site_analyzer, 'rank%d' % rank)
+        for iteration, iter_samples in samples.groupby('iteration'):
+            analyzer_data = self.get_analyzer_data(iteration, site_name, analyzer_name)
+
+            for rank, sample in iter_samples['sample'].iteritems():  # index is rank
+                fname = os.path.join(self.directory, '%s_%s' % (site_name, analyzer_name), 'rank%d' % rank)
                 fig = plt.figure(fname, figsize=(4, 3))
 
                 analyzer.plot(fig, analyzer_data[str(sample)], '-o', color='#CB5FA4', alpha=1, linewidth=1)
@@ -85,17 +79,18 @@ class SiteDataPlotter(BasePlotter):
                 plt.savefig(fname + '.pdf', format='PDF')
                 plt.close(fig)
 
-    def plot_all(self, site_analyzer, iter_samples, clim):
+    def plot_all(self, site_name, analyzer_name, samples, clim):
 
-        analyzer = self.analyzers[site_analyzer]
+        analyzer = self.get_site_analyzer(site_name, analyzer_name)
 
-        fname = os.path.join(self.plots_directory, '%s_all' % site_analyzer)
+        fname = os.path.join(self.directory, '%s_%s_all' % (site_name, analyzer_name))
         fig = plt.figure(fname, figsize=(4, 3))
         cmin, cmax = clim
 
-        for iteration, samples in iter_samples.items():
-            analyzer_data = self.state_for_iteration(iteration).analyzers[site_analyzer]
-            for sample, result in zip(samples['sample'], samples['result']):
+        for iteration, iter_samples in samples.groupby('iteration'):
+            analyzer_data = self.get_analyzer_data(iteration, site_name, analyzer_name)
+            results_by_sample = iter_samples.reset_index().set_index('sample')['total']
+            for sample, result in results_by_sample.iteritems():
                 analyzer.plot(fig, analyzer_data[str(sample)], '-',
                               color=cm.Blues((result - cmin) / (cmax - cmin)), alpha=0.5, linewidth=0.5)
 
@@ -104,18 +99,12 @@ class SiteDataPlotter(BasePlotter):
         plt.savefig(fname + '.pdf', format='PDF')
         plt.close(fig)
 
-    def cleanup_plot(self, calib_manager):
+    def cleanup(self):
         """
         cleanup the existing plots
         :param calib_manager:
         :return:
         """
-
-        # TODO: if this is done in __init__ we don't need to redo it here?
-        self.plots_directory = os.path.join(calib_manager.name, '_plots')  # for directory to remove plots from
-        self.num_to_plot = calib_manager.num_to_plot  # for num_to_plot to remove best_samples
-
-        # TODO: use "glob" to find all plots that match site_analyzer_*.pdf pattern?
         if self.combine_sites:
             for site, analyzers in self.site_analyzer_names.items():
                 self.cleanup_plot_by_analyzers(site, analyzers, self.all_results)
@@ -131,22 +120,22 @@ class SiteDataPlotter(BasePlotter):
         :param samples:
         :return:
         """
-        best_samples = self.get_iteration_samples(samples, subset=True)
+        best_samples = samples.iloc[:self.num_to_plot]
         for analyzer in analyzers:
             site_analyzer = '%s_%s' % (site, analyzer)
             self.cleanup_plot_for_best(site_analyzer, best_samples)
             self.cleanup_plot_for_all(site_analyzer)
 
-    def cleanup_plot_for_best(self, site_analyzer, iter_samples):
+    def cleanup_plot_for_best(self, site_analyzer, samples):
         """
         cleanup the existing plots
         :param site_analyzer:
-        :param iter_samples:
+        :param samples:
         :return:
         """
-        for iteration, samples in iter_samples.items():
-            for sample, rank in zip(samples['sample'], samples['rank']):
-                fname = os.path.join(self.plots_directory, site_analyzer, 'rank%d' % rank)
+        for iteration, iter_samples in samples.groupby('iteration'):
+            for rank, sample in iter_samples['sample'].iteritems():  # index is rank
+                fname = os.path.join(self.directory, site_analyzer, 'rank%d' % rank)
                 plot_path = fname + '.pdf'
                 if os.path.exists(plot_path):
                     try:
@@ -162,7 +151,7 @@ class SiteDataPlotter(BasePlotter):
         :param site_analyzer:
         :return:
         """
-        fname = os.path.join(self.plots_directory, '%s_all' % site_analyzer)
+        fname = os.path.join(self.directory, '%s_all' % site_analyzer)
         plot_path = fname + '.pdf'
         if os.path.exists(plot_path):
             try:
