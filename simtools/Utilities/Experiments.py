01666d629b56f7ca5f4c6a9d3211c873969323d5
Benoit Raybaud <braybaud@intven.com>
2017-01-27 18:03:20 -0800

    Fix #365

--- simtools/Utilities/Experiments.py
+++ simtools/Utilities/Experiments.py
@@ -2,11 +2,13 @@ from __future__ import print_function
 
 from simtools.DataAccess.DataStore import DataStore
 from simtools.SetupParser import SetupParser
+from simtools.Utilities.General import utc_to_local
 from simtools.utils import init_logging
 
 logger = init_logging('DataStore')
 
-def retrieve_experiment(exp_id, sync_if_missing=True):
+
+def retrieve_experiment(exp_id, sync_if_missing=True, verbose=False):
     """
     Retrieve an experiment in the local database based on its id.
     Can call a sync if missing if the flag is true.
@@ -21,14 +23,23 @@ def retrieve_experiment(exp_id, sync_if_missing=True):
         raise Exception('Experiment %s not found in the local database and sync disabled.' % exp_id)
 
     logger.info('Experiment with id %s not found in local database, trying sync.' % exp_id)
-    exp = COMPS_experiment_to_local_db(exp_id)
+    endpoint = SetupParser().lookup_param('HPC', 'server_endpoint')
+    exp = COMPS_experiment_to_local_db(exp_id, endpoint, verbose)
+
     if exp: return exp
     raise Exception("Experiment %s could not be retrieved." % exp_id)
 
 
-def COMPS_experiment_to_local_db(exp_id, endpoint, verbose=False):
+def COMPS_experiment_to_local_db(exp_id, endpoint, verbose=False, save_new_experiment=True):
     """
-    Create a new experiment in local db given COMPS experiment id
+    Return a DB object representing an experiment coming from COMPS.
+    This function saves the newly retrieved experiment in the DB by default but this behavior can be changed by switching
+    the save_new_experiment parameter allowing to return an experiment object and save later with a batch for example.
+    :param exp_id:
+    :param endpoint:
+    :param verbose:
+    :param save_new_experiment:
+    :return:
     """
     # IF the experiment already exists and
     experiment = DataStore.get_experiment(exp_id)
@@ -52,7 +63,7 @@ def COMPS_experiment_to_local_db(exp_id, endpoint, verbose=False):
         experiment = DataStore.create_experiment(exp_id=str(exp_comps.id),
                                                  suite_id=str(exp_comps.suite_id) if exp_comps.suite_id else None,
                                                  exp_name=exp_comps.name,
-                                                 date_created=exp_comps.date_created,
+                                                 date_created=utc_to_local(exp_comps.date_created).replace(tzinfo=None),
                                                  location='HPC',
                                                  selected_block='HPC',
                                                  endpoint=endpoint)
@@ -76,8 +87,11 @@ def COMPS_experiment_to_local_db(exp_id, endpoint, verbose=False):
         simulation = DataStore.create_simulation(id=str(sim.id),
                                                  status=sim.state.name,
                                                  tags=sim.tags,
-                                                 date_created=sim.date_created)
+                                                 date_created=utc_to_local(sim.date_created).replace(tzinfo=None))
         # Add to the experiment
         experiment.simulations.append(simulation)
+
+    # Save it to the DB
+    if save_new_experiment: DataStore.save_experiment(experiment)
 
     return experiment
