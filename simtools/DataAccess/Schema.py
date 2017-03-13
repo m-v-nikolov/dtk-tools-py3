1350f0f71106cbf6fa6876e2706ef2e64fe812f6
Benoit Raybaud <braybaud@intven.com>
2017-03-13 09:41:37 -0700

    Fix #841

--- simtools/DataAccess/Schema.py
+++ simtools/DataAccess/Schema.py
@@ -55,7 +55,7 @@ class Simulation(Base):
             return os.path.join(self.experiment.sim_root, '%s_%s' % (self.experiment.exp_name, self.experiment.exp_id), self.id)
         else:
             from simtools.OutputParser import CompsDTKOutputParser
-            if not CompsDTKOutputParser.sim_dir_map or  not self.id in CompsDTKOutputParser.sim_dir_map:
+            if not CompsDTKOutputParser.sim_dir_map or not self.id in CompsDTKOutputParser.sim_dir_map:
                 CompsDTKOutputParser.createSimDirectoryMap(exp_id=self.experiment.exp_id, save=True)
             return CompsDTKOutputParser.sim_dir_map[self.id]
 
