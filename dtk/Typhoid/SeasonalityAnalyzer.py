b1bbd0149d7ef83c0209b8e281ce1a327b4d744e
Daniel Klein <djklein@gmail.com>
2017-03-28 12:13:57 -0700

    Improvements to seasonality analyzer.

--- dtk/Typhoid/SeasonalityAnalyzer.py
+++ dtk/Typhoid/SeasonalityAnalyzer.py
@@ -9,6 +9,7 @@ import shelve
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib import gridspec
+from matplotlib import collections as mc
 
 from calibtool import LL_calculators
 from dtk.Typhoid.ReportTyphoidInsetChartAnalyzer import ReportTyphoidInsetChartAnalyzer
@@ -47,7 +48,7 @@ class SeasonalityAnalyzer(ReportTyphoidInsetChartAnalyzer):
         self.reference = site.reference_data[self.reference_sheet].set_index(['Year', 'Month', 'AgeBin'])
 
         year_bins = sorted(self.reference.index.get_level_values('Year').unique().tolist())
-        self.ref_pop = site.reference_data['PopulationByAge'].groupby('Year')['Pop'].sum().loc[year_bins[0]:year_bins[-1]+2].sum()
+        self.ref_pop = site.reference_data['PopulationByAge'].groupby('Year')['Population'].sum().loc[year_bins[0]:year_bins[-1]+2].sum()
         self.year_bins = ['[%d, %d)'%(min(year_bins), max(year_bins)+1)]
         self.reference = self.reference.groupby(level=['Month', 'AgeBin']).sum()
         self.reference['Year'] = self.year_bins[0]
@@ -129,7 +130,6 @@ class SeasonalityAnalyzer(ReportTyphoidInsetChartAnalyzer):
         self.data.set_index('Month', append=True, inplace=True)
         #self.data.sort_index(level='Month', sort_remaining=False, inplace=True)
         self.data.sort_index(level='Sample', inplace=True, sort_remaining=True)
-        print self.data.index
 
         d = self.data.reset_index()
         self.cache_data['Sim'] = d.where((pd.notnull(d)), None).to_dict(orient='list')
@@ -158,18 +158,71 @@ class SeasonalityAnalyzer(ReportTyphoidInsetChartAnalyzer):
 
 
     def compare(self, sample):
-        print sample
         LL = sample.reset_index().groupby(['Sim_Id']).apply(self.compare_age)
         return sum(LL.values)
+
+    def make_collection(self, d):
+        return zip(d['MonthInt'], d['Sim_Acute'])
 
 
     def finalize(self):
         super(SeasonalityAnalyzer, self).finalize() # Closes the shelve file
 
+        if '_iter' in self.exp_name:
+            toks = self.exp_name.split('_iter')
+            self.basedir = toks[0]
+            iterdir = 'iter%d'%int(float(toks[1]))
+            figdir = os.path.join(self.basedir, iterdir, self.name) # TODO! # calib_manager.iteration_directory()
+        else:
+            figdir = os.path.join(self.working_dir, self.basedir, self.exp_id, self.name)
+        if not os.path.isdir(figdir):
+            os.mkdir(figdir)
+
+
+        ''' PUT ME BACK!
         writer = pd.ExcelWriter(os.path.join(self.workdir,'Results_%s.xlsx'%self.__class__.__name__))
         #self.result.to_frame().sort_index().to_excel(writer, sheet_name='Result')
         self.data.to_excel(writer, sheet_name=self.__class__.__name__, merge_cells=False)
         writer.save()
+        '''
+
+        import calendar
+        month_to_int_dict = {v: k for k,v in enumerate(calendar.month_name)}
+
+        self.reference.reset_index(inplace=True)
+        self.reference['MonthInt'] = self.reference['Month'].map(month_to_int_dict)
+
+        self.data.reset_index(inplace=True)
+        self.data['MonthInt'] = self.data['Month'].map(month_to_int_dict)
+
+        fig, ax = plt.subplots(1, 1, figsize=(16,10))
+        data_by_sim_id = self.data.groupby('Sim_Id')
+        lc = mc.LineCollection( data_by_sim_id.apply(self.make_collection), linewidths=0.1, cmap=plt.cm.jet )
+        ax.add_collection(lc)
+
+        ax.plot(self.reference['MonthInt'], self.reference['Cases'], 'k.', ms=25)
+
+        ax.autoscale()
+        ax.margins(0.1)
+        ax.set_title('Seasonality')
+
+        fig.savefig(os.path.join(figdir, 'Seasonality.%s'%self.fig_format));
+        plt.close(fig)
+
+        '''
+        lc = mc.LineCollection( data_g_by_sim_id.apply(self.make_collection), linewidths=0.1, cmap=plt.cm.jet )
+        #lc.set_array( data_g_by_sim_id['Results'].apply(lambda z:z)) # <-- Hopefully same order?
+
+        a.add_collection(lc)
+
+        # Use Count to make a poisson confidence interval (noraml approx)
+        a.plot(ref_g['Year'], ref_g['NationalPrevalence'], 'k.', ms=25)
+
+        a.autoscale()
+        a.margins(0.1)
+        a.set_title('%s: %s'%(gender,agebin))
+        '''
+
 
         ''' # WORK IN PROGRESS:
         self.result = self.data.groupby(level='Sample').apply(self.compare)
