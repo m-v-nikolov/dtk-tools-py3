b26e5a3c7f1fed32d17aa81e1f41a83017eea2e4
Jaline Gerardin <jgerardin@intven.com>
2017-03-29 13:31:01 -0700

    add health seeking by CHW

--- dtk/interventions/health_seeking.py
+++ dtk/interventions/health_seeking.py
@@ -1,3 +1,16 @@
+receiving_drugs_event = {
+    "class": "BroadcastEvent",
+    "Broadcast_Event": "Received_Treatment"
+}
+expire_recent_drugs = {"class": "PropertyValueChanger",
+                       "Target_Property_Key": "DrugStatus",
+                       "Target_Property_Value": "RecentDrug",
+                       "Daily_Probability": 1.0,
+                       "Maximum_Duration": 0,
+                       'Revert': 0
+                       }
+
+
 def add_health_seeking(config_builder,
                        start_day=0,
                        # Note: potential for overlapping drug treatments in the same individual
@@ -37,51 +50,14 @@ def add_health_seeking(config_builder,
     :return:
     """
 
-    receiving_drugs_event = {
-        "class": "BroadcastEvent",
-        "Broadcast_Event": "Received_Treatment"
-    }
-    expire_recent_drugs = {  "class": "PropertyValueChanger",
-                             "Target_Property_Key" : "DrugStatus",
-                             "Target_Property_Value" : "RecentDrug",
-                             "Daily_Probability": 1.0,
-                             "Maximum_Duration": 0,
-                             'Revert': drug_ineligibility_duration
-                           }
+    expire_recent_drugs['Revert'] = drug_ineligibility_duration
 
-    # if drug variable is a list, let's use MultiInterventionDistributor
-    if isinstance(drug, basestring):
-        # print('Just a single drug: ' + drug)
-        drug_config = {"Cost_To_Consumer": 1,
-                       "Drug_Type": drug,
-                       "Dosing_Type": dosing,
-                       "class": "AntimalarialDrug"}
-    else:
-        # print('Multiple drugs: ' + '+'.join(drug))
-        drugs = []
-        for d in drug:
-            drugs.append({"Cost_To_Consumer": 1,
-                          "Drug_Type": d,
-                          "Dosing_Type": dosing,
-                          "class": "AntimalarialDrug"})
-        drugs.append(receiving_drugs_event)
-        if drug_ineligibility_duration > 0 :
-            drugs.append(expire_recent_drugs)
-        drug_config = {"class": "MultiInterventionDistributor",
-                       "Intervention_List": drugs}
+    drug_config, drugs = get_drug_config(drug, dosing, receiving_drugs_event,
+                                         drug_ineligibility_duration, expire_recent_drugs)
 
     for t in targets:
 
-        if t['rate'] > 0:
-            actual_config = {
-                "class": "DelayedIntervention",
-                "Coverage": 1.0,
-                "Delay_Distribution": "EXPONENTIAL_DURATION",
-                "Delay_Period": 1.0 / t['rate'],
-                "Actual_IndividualIntervention_Configs": drugs
-            }
-        else:
-            actual_config = drug_config
+        actual_config = build_actual_treatment_cfg(t['rate'], drug_config, drugs)
 
         health_seeking_config = {
             "class": "StandardInterventionDistributionEventCoordinator",
@@ -115,3 +91,107 @@ def add_health_seeking(config_builder,
                                 "Nodeset_Config": nodes}
 
         config_builder.add_event(health_seeking_event)
+
+
+def add_health_seeking_by_chw( config_builder,
+                               start_day=0,
+                               targets={'triggers': ['NewClinicalCase', 'NewSevereCase'],
+                                        'coverage': 0.5, 'agemin': 0, 'agemax': 200, 'rate': 0.3},
+                               drug=['Artemether', 'Lumefantrine'],
+                               dosing='FullTreatmentNewDetectionTech',
+                               nodeIDs=[],
+                               node_property_restrictions=[],
+                               drug_ineligibility_duration=0,
+                               duration=100000,
+                               chw={}):
+
+    chw_config = {
+        'class' : 'CommunityHealthWorkerEventCoordinator',
+        'Duration' : duration,
+        'Distribution_Rate' : 5,
+        'Waiting_Period' : 7,
+        'Days_Between_Shipments' : 90,
+        'Amount_In_Shipment' : 1000,
+        'Max_Stock' : 1000,
+        'Initial_Amount_Distribution_Type' : 'FIXED_DURATION',
+        'Initial_Amount' : 1000,
+        'Target_Demographic' : 'Everyone',
+        'Target_Residents_Only' : 0,
+        'Demographic_Coverage' : targets['coverage'],
+        'Trigger_Condition_List' : targets['triggers'],
+        'Property_Restrictions_Within_Node' : []}
+
+    if chw :
+        chw_config.update(chw)
+
+    if drug_ineligibility_duration > 0:
+        chw_config["Property_Restrictions_Within_Node"].append({"DrugStatus": "None"})
+
+    # NOTE: node property restrictions isn't working yet for CHWEC (3/29/17)
+    if node_property_restrictions:
+        chw_config['Node_Property_Restrictions'] = node_property_restrictions
+
+    if all([k in targets.keys() for k in ['agemin', 'agemax']]):
+        chw_config.update({
+            "Target_Demographic": "ExplicitAgeRanges",  # Otherwise default is Everyone
+            "Target_Age_Min": targets['agemin'],
+            "Target_Age_Max": targets['agemax']})
+
+    expire_recent_drugs['Revert'] = drug_ineligibility_duration
+    drug_config, drugs = get_drug_config(drug, dosing, receiving_drugs_event,
+                                         drug_ineligibility_duration, expire_recent_drugs)
+    actual_config = build_actual_treatment_cfg(targets['rate'], drug_config, drugs)
+
+    chw_config['Intervention_Config'] = actual_config
+
+    nodes = {"class": "NodeSetNodeList", "Node_List": nodeIDs} if nodeIDs else {"class": "NodeSetAll"}
+
+    chw_event = {"class": "CampaignEvent",
+                 "Start_Day": start_day,
+                 "Event_Coordinator_Config": chw_config,
+                 "Nodeset_Config": nodes}
+
+    config_builder.add_event(chw_event)
+
+
+def get_drug_config(drug, dosing, receiving_drugs_event, drug_ineligibility_duration, expire_recent_drugs) :
+
+    # if drug variable is a list, let's use MultiInterventionDistributor
+    if isinstance(drug, basestring):
+        # print('Just a single drug: ' + drug)
+        drug_config = {"Cost_To_Consumer": 1,
+                       "Drug_Type": drug,
+                       "Dosing_Type": dosing,
+                       "class": "AntimalarialDrug"}
+        drugs = drug
+    else:
+        # print('Multiple drugs: ' + '+'.join(drug))
+        drugs = []
+        for d in drug:
+            drugs.append({"Cost_To_Consumer": 1,
+                          "Drug_Type": d,
+                          "Dosing_Type": dosing,
+                          "class": "AntimalarialDrug"})
+        drugs.append(receiving_drugs_event)
+        if drug_ineligibility_duration > 0 :
+            drugs.append(expire_recent_drugs)
+        drug_config = {"class": "MultiInterventionDistributor",
+                       "Intervention_List": drugs}
+
+    return drug_config, drugs
+
+
+def build_actual_treatment_cfg(rate, drug_config, drugs) :
+
+    if rate > 0:
+        actual_config = {
+            "class": "DelayedIntervention",
+            "Coverage": 1.0,
+            "Delay_Distribution": "EXPONENTIAL_DURATION",
+            "Delay_Period": 1.0 / rate,
+            "Actual_IndividualIntervention_Configs": drugs
+        }
+    else:
+        actual_config = drug_config
+
+    return actual_config
