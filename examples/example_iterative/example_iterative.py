7e119cdf24a2f253bab6a498f1778b08e809d0bd
Benoit Raybaud <braybaud@intven.com>
2017-04-21 20:56:09 -0700

    Cleanup a bit the logic to retrieve results from the IterationState. Also fix the resume capability of the iterative next point algorithm

--- examples/example_iterative/example_iterative.py
+++ examples/example_iterative/example_iterative.py
@@ -119,5 +119,9 @@ calib_manager = CalibManager(name=sim_name,
 run_calib_args = {}
 
 if __name__ == "__main__":
+    # The following line would resume from iteration 0 at the step analyze
+    # The available steps are: commission, analyze, next_point
+    # calib_manager.resume_from_iteration(iteration=0, iter_step='analyze')
+    # For now cleanup automatically
     calib_manager.cleanup()
     calib_manager.run_calibration(**run_calib_args)
