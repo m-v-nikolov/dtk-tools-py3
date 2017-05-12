3f7626878e86a11993e3f8111cc61f8bdb8e1fc8
Benoit Raybaud <braybaud@intven.com>
2017-05-12 11:28:34 -0700

    Fix for the orphans deletion

--- simtools/DataAccess/ExperimentDataStore.py
+++ simtools/DataAccess/ExperimentDataStore.py
@@ -19,9 +19,9 @@ class ExperimentDataStore:
         return Experiment(**kwargs)
 
     @classmethod
-    def get_experiment(cls, exp_id):
+    def get_experiment(cls, exp_id, current_session=None):
         logger.debug("Get Experiment")
-        with session_scope() as session:
+        with session_scope(current_session) as session:
             # Get the experiment
             # Also load the associated simulations eagerly
             experiment = session.query(Experiment).options(
@@ -138,7 +138,9 @@ class ExperimentDataStore:
         """
         exp_list = None
         with session_scope() as session:
-            exp_list = session.query(Experiment).filter(Experiment.suite_id.in_(suite_ids)).all()
+            exp_ids = session.query(Experiment.exp_id).filter(Experiment.suite_id.in_(suite_ids)).all()
+            # Retrieve the individual experiments
+            exp_list = [cls.get_experiment(exp[0], session) for exp in exp_ids]
             session.expunge_all()
 
         return exp_list
