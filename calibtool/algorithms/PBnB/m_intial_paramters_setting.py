6c4ab34001767396a7a1fb703bc16f4e0e2b8778
Benoit Raybaud <braybaud@intven.com>
2017-08-31 14:11:43 -0700

    Add PBnB to the list of algorithms (example will follow)

---
+++ calibtool/algorithms/PBnB/m_intial_paramters_setting.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 23 22:15:00 2017
+
+@author: TingYu Ho
+"""
+
+f_delta = 0.2
+f_alpha = 0.5
+i_k_b = 4
+i_n_branching = 2
+i_c = 50
+i_replication = 1
+i_stopping_max_k = 10  # maximum allowable iteration
+i_num_simulation_per_run = 1000
+
