972caa16eba61aa69613c530975df10a529c52b5
zdu <zdu@intven.com>
2017-09-11 09:46:06 -0700

    #1122: complete the implementation and also update one test

--- simtools/AnalyzeManager/AnalyzeManager.py
+++ simtools/AnalyzeManager/AnalyzeManager.py
@@ -18,9 +18,11 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 class AnalyzeManager:
-
-    def __init__(self, exp_list=[], analyzers=[], working_dir=None, force_analyze=False, verbose=True, create_dir_map=True):
+    def __init__(self, exp_list=[], sim_list=[], analyzer_list=[], working_dir=None, force_analyze=False, verbose=True,
+                 create_dir_map=True):
         self.experiments = []
+        self.simulations = []
+        self.experiments_simulations = {}
         self.verbose = verbose
         self.analyzers = []
         with SetupParser.TemporarySetup() as sp:
@@ -39,8 +41,12 @@ class AnalyzeManager:
         map(self.add_experiment, exp_list)
 
         # Initial adding of the analyzers
-        analyzers = analyzers if isinstance(analyzers, list) else [analyzers]
-        map(self.add_analyzer, analyzers)
+        sim_list = sim_list if isinstance(sim_list, list) else [sim_list]
+        map(self.add_simulation, sim_list)
+
+        # Initial adding of the analyzers
+        analyzer_list = analyzer_list if isinstance(analyzer_list, list) else [analyzer_list]
+        map(self.add_analyzer, analyzer_list)
 
     def add_experiment(self, experiment):
         from simtools.DataAccess.Schema import Experiment
@@ -49,6 +55,18 @@ class AnalyzeManager:
 
         if experiment not in self.experiments:
             self.experiments.append(experiment)
+
+    def add_simulation(self, simulation):
+        from simtools.DataAccess.Schema import Simulation
+        if not isinstance(simulation, Simulation):
+            simulation = DataStore.get_simulation(simulation)
+
+        experiment = simulation.experiment
+
+        if experiment.exp_id not in self.experiments_simulations:
+            self.experiments_simulations[experiment.exp_id] = [simulation]
+        else:
+            self.experiments_simulations[experiment.exp_id].append(simulation)
 
     def add_analyzer(self, analyzer, working_dir=None):
         analyzer.working_dir = working_dir or self.working_dir
@@ -82,8 +100,17 @@ class AnalyzeManager:
         # Create the thread pool to create the parsers
         p = ThreadPool()
         res = []
-        for simulation in exp_manager.experiment.simulations:
-            res.append(p.apply_async(self.parser_for_simulation, args=(simulation, experiment, exp_manager)))
+
+        if experiment.exp_id in self.experiments_simulations:
+            # only consider simulations provided
+            for simulation in self.experiments_simulations[experiment.exp_id]:
+                res.append(p.apply_async(self.parser_for_simulation, args=(simulation, experiment, exp_manager)))
+
+            # drop experiment from self.experiments_simulations
+            self.experiments_simulations.pop(experiment.exp_id)
+        else:
+            for simulation in exp_manager.experiment.simulations:
+                res.append(p.apply_async(self.parser_for_simulation, args=(simulation, experiment, exp_manager)))
 
         p.close()
         p.join()
@@ -97,6 +124,37 @@ class AnalyzeManager:
                 data = True
         if data:
             self.experiments_with_data.append(experiment)
+
+    def create_parsers_for_experiment_from_simulation(self, exp_id):
+        experiment = retrieve_experiment(exp_id)
+        self.create_parsers_for_experiment(experiment)
+
+    def create_parser_for_simulation(self, simulation):
+        experiment = simulation.experiment
+
+        # Create a manager for the current experiment
+        exp_manager = ExperimentManagerFactory.from_experiment(experiment)
+
+        # Refresh the experiment just to be sure to have latest info
+        exp_manager.refresh_experiment()
+
+        if exp_manager.location == 'HPC':
+            # Get the sim map no matter what
+            if self.create_dir_map:
+                exp_manager.parserClass.createSimDirectoryMap(exp_id=exp_manager.experiment.exp_id,
+                                                              suite_id=exp_manager.experiment.suite_id,
+                                                              save=True, comps_experiment=exp_manager.comps_experiment,
+                                                              verbose=self.verbose)
+            if not exp_manager.asset_service:
+                exp_manager.parserClass.asset_service = False
+
+        # Call the analyzer per experiment function for initialization
+        for analyzer in self.analyzers:
+            analyzer.per_experiment(experiment)
+
+        # Create a parser for given simulation
+        parser = self.parser_for_simulation(simulation, experiment, exp_manager)
+        self.parsers.append(parser)
 
     def parser_for_simulation(self, simulation, experiment, manager):
         # If simulation not done -> return none
@@ -137,14 +195,17 @@ class AnalyzeManager:
         # Create the parsers for the experiments
         map(self.create_parsers_for_experiment, self.experiments)
 
+        # Create the parsers for the experiments
+        map(self.create_parsers_for_experiment_from_simulation, self.experiments_simulations.keys())
+
         # only process experiments that have at least some data in them
         for experiment in self.experiments:
             if experiment not in self.experiments_with_data: # this is set in self.create_parsers_for_experiment
                 print('Experiment %s has no simulations ready for analysis, skipping...' % experiment.exp_id)
                 self.experiments.remove(experiment)
 
-        if len(self.experiments) == 0:
-            print('No experiments have data ready for analysis.')
+        if len(self.parsers) == 0:
+            print('No experiments/simulations for analysis.')
             return # mimic the above len(self.analyzers) == 0 behavior
 
         for parser in self.parsers:
