de24a536dddc97c2f2863ab11d2ccb86b66717e6
Jaline Gerardin <jgerardin@intven.com>
2017-09-28 11:10:50 -0700

    biting risk param name clarification triggering option to add_ITN

--- dtk/interventions/itn.py
+++ dtk/interventions/itn.py
@@ -28,7 +28,7 @@ receiving_itn_event = {
 
 
 def add_ITN(config_builder, start, coverage_by_ages, waning={}, cost=None, nodeIDs=[], node_property_restrictions=[],
-            ind_property_restrictions=[]):
+            ind_property_restrictions=[], trigger_string=''):
     """
     Add an ITN intervention to the config_builder passed.
     :param config_builder: The :py:class:`DTKConfigBuilder <dtk.utils.core.DTKConfigBuilder>` holding the campaign that will receive the ITN event
@@ -55,24 +55,42 @@ def add_ITN(config_builder, start, coverage_by_ages, waning={}, cost=None, nodeI
 
     for coverage_by_age in coverage_by_ages:
 
-        ITN_event = { "class" : "CampaignEvent",
-                      "Start_Day": int(start),
-                      "Event_Coordinator_Config": {
-                          "class": "StandardInterventionDistributionEventCoordinator",
-                          'Node_Property_Restrictions': [],
-                          "Target_Residents_Only" : 1,
-                          "Demographic_Coverage": coverage_by_age["coverage"],
-                          "Intervention_Config": itn_bednet_w_event #itn_bednet
-                      }
-                    }
+        ITN_event = {"class": "CampaignEvent",
+                     "Start_Day": int(start),
+                     "Event_Coordinator_Config": {
+                         "class": "StandardInterventionDistributionEventCoordinator",
+                         'Node_Property_Restrictions': []
+                         }
+                     }
+
+        if trigger_string:
+            ITN_event['Event_Coordinator_Config']['Intervention_Config'] = {
+                "class" : "NodeLevelHealthTriggeredIV",
+                "Trigger_Condition_List": [trigger_string],
+                "Target_Residents_Only": 1,
+                "Demographic_Coverage": coverage_by_age["coverage"],
+                "Actual_IndividualIntervention_Config" : itn_bednet_w_event
+            }
+
+        else :
+            ITN_event['Event_Coordinator_Config'].update( {
+                "Target_Residents_Only": 1,
+                "Demographic_Coverage": coverage_by_age["coverage"],
+                'Intervention_Config' : itn_bednet_w_event
+            })
+
         if node_property_restrictions:
             ITN_event['Event_Coordinator_Config']['Node_Property_Restrictions'].extend(node_property_restrictions)
 
         if all([k in coverage_by_age.keys() for k in ['min','max']]):
-            ITN_event["Event_Coordinator_Config"].update({
+            target_by_age = {
                    "Target_Demographic": "ExplicitAgeRanges",
                    "Target_Age_Min": coverage_by_age["min"],
-                   "Target_Age_Max": coverage_by_age["max"]})
+                   "Target_Age_Max": coverage_by_age["max"]}
+            if trigger_string :
+                ITN_event["Event_Coordinator_Config"]['Intervention_Config'].update(target_by_age)
+            else :
+                ITN_event["Event_Coordinator_Config"].update(target_by_age)
 
         if not nodeIDs:
             ITN_event["Nodeset_Config"] = { "class": "NodeSetAll" }
@@ -95,6 +113,9 @@ def add_ITN(config_builder, start, coverage_by_ages, waning={}, cost=None, nodeI
                 ITN_event["Event_Coordinator_Config"]["Intervention_Config"]["Property_Restrictions_Within_Node"] = ind_property_restrictions
 
         elif ind_property_restrictions:
-            ITN_event["Event_Coordinator_Config"]["Property_Restrictions_Within_Node"] = ind_property_restrictions
+            if trigger_string :
+                ITN_event["Event_Coordinator_Config"]['Intervention_Config']["Property_Restrictions_Within_Node"] = ind_property_restrictions
+            else :
+                ITN_event["Event_Coordinator_Config"]["Property_Restrictions_Within_Node"] = ind_property_restrictions
 
         config_builder.add_event(ITN_event)
