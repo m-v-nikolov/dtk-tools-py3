1d5e883e0355f0147d50307d72e59b58cf772b1c
zdu <zdu@intven.com>
2017-10-25 10:35:24 -0700

    Work on resume IMIS (still non functioning)

--- calibtool/algorithms/IMIS.py
+++ calibtool/algorithms/IMIS.py
@@ -464,3 +464,7 @@ class IMIS(NextPointAlgorithm):
         self.gaussian_probs = {}
         self.gaussian_covariances = []
         self.gaussian_centers = []
+
+    def restore(self, iteration_state):
+        self.gaussian_covariances = iteration_state.next_point['gaussian_covariances']
+        self.gaussian_centers = iteration_state.next_point['gaussian_centers']
