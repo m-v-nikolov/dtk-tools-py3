cda7c87d454760a9f769f8280d00747f197fe50a
Clark Kirkman IV <ckirkman@idmod.org>
2017-12-13 14:55:50 -0800

    First stab at making the calibration script look like a dtk run script + hand-made sweep builder for the given points (param overrides)

--- calibtool/ResampleManager.py
+++ calibtool/ResampleManager.py
@@ -5,12 +5,15 @@ class ResampleManager:
         self.steps = steps
         self.analyzer_path = analyzer_path
 
-    def resample_and_run(self, calibrated_points, run_args, unknown_args):
+    def resample_and_run(self, calibrated_points, resample_step, run_args, unknown_args):
+        resample_step = 0
         for resampler in self.steps:
             calibrated_points = resampler.resample_and_run(calibrated_points=calibrated_points,
                                                            analyzer_path=self.analyzer_path,
+                                                           resample_step=resample_step,
                                                            run_args=run_args,
                                                            unknown_args=unknown_args)
+            resample_step += 1
         self.results = calibrated_points
 
     def write_results(self, filename):
--- calibtool/resamplers/Resampler.py
+++ calibtool/resamplers/Resampler.py
@@ -1,6 +1,7 @@
-import dtk.commands
 from argparse import Namespace
 
+import dtk.commands
+from dtk.utils.builders.sweep import GenericSweepBuilder
 
 class Resampler:
     def __init__(self):
@@ -9,16 +10,25 @@ class Resampler:
     def _resample(self, calibrated_points):
         raise Exception('Subclasses of Resampler must define their own _resample() method')
 
-    def _run(self, points, run_args, unknown_args):
+    def _run(self, points, resample_step, run_args, unknown_args):
         """
         This run method is for running simulations, which is the in-common part of resampling.
         :param points: The points to run simulations at.
         :return: The Experiment object for these simulations
         """
-        run_args = run_args if run_args else {}
         run_args.blocking = True
 
-        # ck4, incorporate points into run_args somehow HERE
+        # create a sweep where each point is a separate sim to perform and make the calibration script look like
+        # a dtk tools run script
+        overrides_list = [point.to_dict() for point in points]
+        sweep_builder = GenericSweepBuilder.from_list_of_override_dicts(overrides_list)
+        mod = run_args.loaded_module
+        run_sim_args = {
+            'exp_name': mod.calib_manager.name + '_resample_step_%d' % resample_step,
+            'exp_builder': sweep_builder,
+            'config_builder': mod.calib_manager.config_builder
+        }
+        setattr(mod, 'run_sim_args', run_sim_args)
 
         # run simulations at the provided points
         experiment = dtk.commands.run(args=run_args, unknownArgs=unknown_args)
@@ -44,7 +54,7 @@ class Resampler:
         resampled_points = some_stuff # ck4
         return resampled_points
 
-    def resample_and_run(self, calibrated_points, analyzer_path, run_args, unknown_args):
+    def resample_and_run(self, calibrated_points, analyzer_path, resample_step, run_args, unknown_args):
         """
         Canonical entry method for using the resampler.
         :param calibrated_points:
@@ -54,7 +64,8 @@ class Resampler:
         points_to_run = self._resample(calibrated_points=calibrated_points)
 
         # 2. run simulations
-        self.experiment = self._run(points=points_to_run, run_args=run_args, unknown_args=unknown_args)
+        self.experiment = self._run(points=points_to_run, resample_step=resample_step,
+                                    run_args=run_args, unknown_args=unknown_args)
 
         # 3. analyze simulations for liklihood
         self.resampled_points = self._analyze(experiment=self.experiment, analyzer_path=analyzer_path,
