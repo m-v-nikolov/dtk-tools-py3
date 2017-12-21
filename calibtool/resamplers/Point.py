df6b523c4fdede6ec38eb35da9ed6cc3b09d6ccb
zdu <zdu@intven.com>
2017-12-21 11:29:34 -0800

    #1282: cleanup calibrated point (removed comments)

--- calibtool/resamplers/Point.py
+++ calibtool/resamplers/Point.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pandas as pd
+
 
 class CalibrationPoint:
     def __init__(self, parameters=None, likelihood=None):
@@ -21,8 +21,6 @@ class CalibrationPoint:
         for p in self.parameters:
             d = p.to_dict()
             d = {k: [v] for k, v in d.items()}
-            # print(p.to_dict())
-            # print(d)
             if df is None:
                 df = pd.DataFrame(d)
             else:
@@ -68,21 +66,5 @@ class CalibrationParameter:
         }
 
     def to_dataframe(self):
-        # pname_list = []
-        # center_list = []
-        # max_list = []
-        # min_list = []
-        # for p in input['parameters']:
-        #     pname_list.append(p['Name'])
-        #     center_list.append(p['Value'])
-        #     max_list.append(p['Max'])
-        #     min_list.append(p['Min'])
-        #
-        # df = pd.DataFrame(data={'Name': pname_list, 'Center': center_list, 'Max': max_list, 'Min': min_list})
-        # df['Center'] = df['Center'].astype('float')
-        # df['Min'] = df['Min'].astype('float')
-        # df['Max'] = df['Max'].astype('float')
-        # print(df)
-
         df = pd.DataFrame(self.to_dict())
         return df
